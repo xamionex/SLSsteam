de9287aeb0914a07487a761bee8b5942da795bfa
AceSLS <ruffy1234.5678@gmail.com>
2025-05-05 12:21:28 +0200

    Added AssemblyWriter function

--- src/memhlp.hpp
+++ src/memhlp.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "libmem/libmem.h"
+#include "log.hpp"
+#include <cstdio>
+#include <cstdlib>
 
 namespace MemHlp
 {
@@ -10,6 +13,33 @@ namespace MemHlp
 		Relative,
 		PrologueUpwards
 	};
+
+	///Summary:
+	///Write assembly code to address and increase address by bytes written
+	template<typename ...Args>
+	bool assembleCodeAt(lm_address_t& address, const char* fmt, Args... args)
+	{
+		size_t size = snprintf(nullptr, 0, fmt, args...) + 1;
+		char* code = reinterpret_cast<char*>(malloc(size));
+		snprintf(code, size, fmt, args...);
+
+		//TODO: Potentially replace with LM_AssembleEx and only allocate memory as needed
+		static lm_inst_t inst;
+		if (!LM_Assemble(code, &inst))
+		{
+			g_pLog->debug("Failed to assemble %s!\n", code);
+			return false;
+		}
+		if (address != LM_ADDRESS_BAD && !LM_WriteMemory(address, inst.bytes, inst.size))
+		{
+			g_pLog->debug("Failed to write %s to %p!\n", code, address);
+			return false;
+		}
+
+		g_pLog->debug("Wrote %s to %p with %i bytes\n", code, address, inst.size);
+		address += inst.size;
+		return true;
+	}
 
 	lm_address_t searchSignature(const char* name, const char* signature, lm_module_t module, SigFollowMode);
 	lm_address_t searchSignature(const char* name, const char* signature, lm_module_t module);
